a88019a064caf32bb61a1d15f21134801651d135
BILON Ikram <bilonikram20@gmail.com>
2025-04-21 10:45:27 +0100

    update cors configs

--- src/index.js
+++ src/index.js
@@ -13,9 +13,10 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "*",  // Allow all origins temporarily (not recommended for production)
+  origin: "https://v25-preprod.harx.ai",  // Allow all origins temporarily (not recommended for production)
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],  // Allowed HTTP methods
-  allowedHeaders: "*"// Allowed request headers
+  allowedHeaders: "*",// Allowed request headers,
+  credentials: true,  // Allows credentials (cookies, authorization headers) to be included in cross-origin requests
 }));
 app.use(express.json());
 
